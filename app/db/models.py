10318ff1d694a0ced8aae93d204bf2f5ba3ee526
iamudaya <usharma4501@gamil.com>
2025-06-30 19:48:16 +0545

    added feedback and log services

--- app/db/models.py
+++ app/db/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Text, JSON
+from sqlalchemy import Column, Integer, String, Text, JSON, DateTime
 from app.db.database import Base
+from datetime import datetime
 
 class ContentModel(Base):
     __tablename__ = "contents"
@@ -10,3 +11,23 @@ class ContentModel(Base):
     grade = Column(String)
     content = Column(Text)
     metadata_ = Column("metadata", JSON)  
+
+
+class FeedbackModel(Base):
+    __tablename__ = "feedback"
+
+
+    id = Column(Integer, primary_key=True)
+    question = Column(String, nullable=False)
+    answer = Column(String, nullable=False)
+    rating = Column(Integer, nullable=False)  
+    comment = Column(String)
+    timestamp = Column(DateTime, default=datetime.utcnow)  
+
+class LogModel(Base):
+    __tablename__ = "logs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    question = Column(String)
+    answer = Column(String)
+    timestamp = Column(DateTime, default=datetime.utcnow) 
