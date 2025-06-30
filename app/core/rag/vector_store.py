24d3c954260e9e361381dd1ef475f42629ce303c
iamudaya <usharma4501@gamil.com>
2025-06-30 12:27:07 +0545

    updated modelname and embeddings

--- app/core/rag/vector_store.py
+++ app/core/rag/vector_store.py
@@ -1,13 +1,22 @@
 import os
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from app.core.config import settings
 
 FAISS_INDEX_PATH = "faiss_index"
 
 def get_vectorstore():
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-001",
+        google_api_key=settings.GOOGLE_API_KEY
+    )
+    
     if os.path.exists(FAISS_INDEX_PATH):
-        embeddings = GoogleGenerativeAIEmbeddings(model="embedding-001")
-        return FAISS.load_local(FAISS_INDEX_PATH, embeddings)
+        return FAISS.load_local(
+            FAISS_INDEX_PATH,
+            embeddings=embeddings,
+            allow_dangerous_deserialization=True
+        )
     else:
         return None
 
